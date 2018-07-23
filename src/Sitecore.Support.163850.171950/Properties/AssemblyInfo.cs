33825efe3d944084f63b704edad80a53e0a57b80
vle <vle@sitecore.net>
2018-07-23 16:29:40 +0300

    Initial

---
+++ src/Sitecore.Support.163850.171950/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.163850.171950")]
+[assembly: AssemblyProduct("Sitecore.Support.163850.171950")]
+[assembly: ComVisible(false)]
